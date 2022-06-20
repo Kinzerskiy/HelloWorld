87abcd9bf5cebc0e4dddc62e156d715fe1cd5ce2
Anton Kinzersky <kinzzersky@gmail..com>
2022-06-20 18:05:28 +0300

    Initial Commit

---
+++ HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Egor on 20.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
